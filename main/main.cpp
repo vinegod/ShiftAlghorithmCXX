b397dfa291676ecd8e8f6370aba15b0220ab030d
vinegod <denkalenichenko@gmail.com>
2023-02-21 00:22:09 +0200

    feat: update simple algorithm test

--- main/main.cpp
+++ main/main.cpp
@@ -4,14 +4,20 @@
 #include <string>
 #include <vector>
 
+void print(std::string str) {
+    std::cout << str << std::endl;
+    for (const auto &c : std::vector<uint8_t>(str.begin(), str.end())) {
+        std::cout << (uint32_t)c << ' ';
+    }
+    std::cout << std::endl;
+}
+
 int main() {
-    auto key_text = std::string("aabaa");
+    auto key_text = std::string("zzzzzz");
     Encoder<std::string> encoder(key_text);
 
-    auto encoded = encoder(std::string("I'm a Panda Bear"));
-    std::cout << encoded << std::endl;
+    auto encoded = encoder(std::string("The quick brown fox jumps over the lazy dog"));
+    print(encoded);
     Decoder<decltype(encoder)::type> decoder(encoder);
-
-    std::cout << decoder(encoded) << std::endl;
-
+    print(decoder(encoded));
 }
