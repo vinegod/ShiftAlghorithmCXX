878c15a50137917e43c1bb280fa905f6ca7dda2c
vinegod <denkalenichenko@gmail.com>
2023-03-06 21:11:36 +0200

    feat: merge `Encoder` and `Decoder` to `Coder`

--- shifter/include/shifter.hpp
+++ shifter/include/shifter.hpp
@@ -6,6 +6,14 @@
 #include <type_traits>
 #include <vector>
 
+/**
+ * @brief Removes from text special symbols and makes it lower case.
+ *
+ * @param text string for transformation
+ * @return std::vector<std::uint8_t>
+ */
+std::vector<std::uint8_t> prepare_text(std::vector<std::uint8_t> &&text);
+
 namespace {
 template <typename T, typename _ = void> struct is_container : std::false_type {};
 template <typename... Ts> struct is_container_helper {};
@@ -18,8 +26,9 @@ template <typename... Ts> struct is_container_helper {};
 template <typename T>
 struct is_container<
     T, std::conditional_t<false,
-                          is_container_helper<typename T::value_type, typename T::size_type, typename T::iterator,
-                                              typename T::const_iterator, decltype(std::declval<T>().size()),
+                          is_container_helper<typename T::value_type, typename T::size_type,
+                                              typename T::iterator, typename T::const_iterator,
+                                              decltype(std::declval<T>().size()),
                                               decltype(std::declval<T>().begin()), decltype(std::declval<T>().end()),
                                               decltype(std::declval<T>().cbegin()), decltype(std::declval<T>().cend())>,
                           void>> : public std::true_type {};
@@ -52,37 +61,26 @@ inline std::string shift_elemets(std::vector<std::uint8_t> original_string, cons
     return {original_string.begin(), original_string.end()};
 }
 
-} // namespace
-
 /**
- * @brief Removes from text special symbols and makes it lower case.
- *
- * @param text string for transformation
- * @return std::vector<std::uint8_t>
+ * @brief Enum class to define Coder's mode
+ * 
  */
-std::vector<std::uint8_t> prepare_text(std::vector<std::uint8_t> &&text);
+enum class CoderMode { Encoder, Decoder };
 
-// Forward declaration of Decoder struct to use as friend in Encoder struct
-template <typename Key, std::enable_if_t<is_container<Key>::value, bool> = true,
-          std::enable_if_t<std::is_integral<typename Key::value_type>::value, bool> = true>
-struct Decoder;
-
-/**
- * @brief Encoder struct for encoding text with Vigenere or Shift algorithm.
- * Uses templates to be able get every std container as input.
- *
- * @tparam Key type of std container
- */
-template <typename Key, std::enable_if_t<is_container<Key>::value, bool> = true,
-          std::enable_if_t<std::is_integral<typename Key::value_type>::value, bool> = true>
-struct Encoder {
+template <typename Key, CoderMode Mode,
+            std::enable_if_t<is_container<Key>::value, bool> = true,
+            std::enable_if_t<std::is_integral<typename Key::value_type>::value, bool> = true>
+struct Coder {
     using type = Key;
+    Coder(const Key &key) : m_key{prepare_text({key.begin(), key.end()})} {};
+    Coder(Key &&key) : m_key{prepare_text({key.begin(), key.end()})} {};
 
-    explicit Encoder(Key key) : m_key{prepare_text({key.begin(), key.end()})} {};
-    explicit Encoder(Decoder<Key> &&encoder) : m_key{encoder.m_key} {};
+    Coder(const Coder<Key, CoderMode::Encoder> &coder) : m_key{coder.m_key} {};
+    Coder(const Coder<Key, CoderMode::Decoder> &coder) : m_key{coder.m_key} {};
+
 
     /**
-     * @brief Operator that encodes original text with given key
+     * @brief Operator that coder original text with given key
      *
      * @tparam Text template parameter that accepts vector<integral_type> and string.
      * @param original_string string that should be encoded.
@@ -90,53 +88,32 @@ struct Encoder {
      */
     template <typename Text, std::enable_if_t<is_container<Key>::value, bool> = true,
               std::enable_if_t<std::is_integral<typename Key::value_type>::value, bool> = true>
+
     std::string operator()(Text original_string) const {
-        const std::int16_t shift_mode = 1;
+
+        // Lambda value initialization
+        const std::int16_t shift_mode = []() {
+            if (Mode == CoderMode::Encoder) {
+                return 1;
+            }
+            return -1;
+        }();
+
         if constexpr (std::is_same<Text, std::vector<uint8_t>>::value) {
-            return shift_elemets(prepare_text(std::forward(original_string)), m_key, shift_mode);
+            return shift_elemets(prepare_text(std::move(original_string)), m_key, shift_mode);
         } else {
             return shift_elemets(prepare_text({original_string.begin(), original_string.end()}), m_key, shift_mode);
         }
     }
 
-    friend struct Decoder<Key>;
+    friend struct Coder<Key, CoderMode::Encoder>;
+    friend struct Coder<Key, CoderMode::Decoder>;
 
   private:
     const std::vector<std::uint8_t> m_key;
 };
 
-/**
- * @brief Decoder struct for decoding text with Vigenere or Shift algorithm.
- * Uses templates to be able get every std container as input.
- *
- * @tparam Key type of std container
- */
-template <typename Key, std::enable_if_t<is_container<Key>::value, bool>,
-          std::enable_if_t<std::is_integral<typename Key::value_type>::value, bool>>
-struct Decoder {
-    explicit Decoder(Key key) : m_key{prepare_text({key.begin(), key.end()})} {};
-    explicit Decoder(const Encoder<Key> &encoder) : m_key{encoder.m_key} {};
+} // namespace
 
-    /**
-     * @brief Operator that decodes original text with given key
-     *
-     * @tparam Text template parameter that accepts vector<integral_type> and string.
-     * @param original_string string that should be decoded.
-     * @return std::string encoded string.
-     */
-    template <typename Text, std::enable_if_t<is_container<Key>::value, bool> = true,
-              std::enable_if_t<std::is_integral<typename Key::value_type>::value, bool> = true>
-    std::string operator()(Text original_string) const {
-        const std::int16_t shift_mode = -1;
-        if constexpr (std::is_same<Text, std::vector<uint8_t>>::value) {
-            return shift_elemets(std::forward(original_string), m_key, shift_mode);
-        } else {
-            return shift_elemets({original_string.begin(), original_string.end()}, m_key, shift_mode);
-        }
-    }
-
-    friend struct Encoder<Key>;
-
-  private:
-    const std::vector<std::uint8_t> m_key;
-};
+template <typename Key> using Encoder = Coder<Key, CoderMode::Encoder>;
+template <typename Key> using Decoder = Coder<Key, CoderMode::Decoder>;
